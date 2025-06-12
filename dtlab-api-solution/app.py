c9eac834e6a58a8ac289e9114aaadb9ca51d9939
giuseppe <capassog97@gmail.com>
2025-06-12 15:42:19 +0200

    Added secret support

--- dtlab-api-solution/app.py
+++ dtlab-api-solution/app.py
@@ -13,7 +13,13 @@ load_dotenv()
 # create flask instance
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv("DB_URI")
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
+db_uri = os.getenv("DB_URI")
+
+with open("/run/secrets/postgres_password", "r") as f:
+    password = f.readline().strip()
+    db_uri = db_uri.replace("__pass__", password)
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
 # db connection ana start application
 from models import db
@@ -26,10 +32,10 @@ with app.app_context():
 
 
 # mount endpoint defined in other files
-app.register_blueprint(router_blueprint, url_prefix='/routers')
-app.register_blueprint(switches_blueprint, url_prefix='/switches')
+app.register_blueprint(router_blueprint, url_prefix="/routers")
+app.register_blueprint(switches_blueprint, url_prefix="/switches")
 
-@app.route('/test', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def test() -> str:
-    return 'Test succeded!'
-
+    return "Test succeded!"
